5c68a560de35d3c6537eda3a85efe46686719b51
funny-family <maksvadik228@gmail.com>
2024-03-09 23:23:17 +0700

    update constants

---
+++ lib/component/date-picker.constants.ts
@@ -0,0 +1,12 @@
+export var MAX_MONTH_NUMBER = 12 as const;
+export var MAX_DAY_NUMBER = 31 as const;
+export var MAX_YEAR_NUMBER = 9999 as const;
+
+export var SEPARATOR_SLASH = '/' as const;
+export var SEPARATOR_PERIOD = '.' as const;
+export var SEPARATOR_DASH = '-' as const;
+export var SEPARATOR_SPACE = ' ' as const;
+
+export var D_REGEXP = /d/is;
+export var M_REGEXP = /m/is;
+export var Y_REGEXP = /y/is;
