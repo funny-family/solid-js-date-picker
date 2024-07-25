8dd0612cdf21831a14da4ca821c5e71091fc7b27
funny-family <maksvadik228@gmail.com>
2024-07-25 15:30:09 +0700

    update

--- playground/src/date-picker1/date-picker.component.tsx
+++ playground/src/date-picker1/date-picker.component.tsx
@@ -1,152 +1,293 @@
-import { createEffect, createSignal, type JSX, on, splitProps } from 'solid-js';
-import type { DatePickerComponent } from './date-picker.component.types';
-import { Portal } from 'solid-js/web';
+import {
+  createSignal,
+  Show,
+  splitProps,
+  type Component,
+  type JSX,
+} from 'solid-js';
 import './date-picker.styles.scss';
 
-var isArray = Array.isArray;
+export var DatePicker = ((attrsAndProps: JSX.IntrinsicElements['input']) => {
+  var [customAttr, inputAttr, rest] = splitProps(
+    attrsAndProps,
+    ['classList', 'ref', '$ServerOnly', 'children'],
+    [
+      'accept',
+      'alt',
+      'autocomplete',
+      'autocorrect',
+      'autofocus',
+      'capture',
+      'checked',
+      'crossorigin',
+      'disabled',
+      'enterkeyhint',
+      'form',
+      'formaction',
+      'formenctype',
+      'formmethod',
+      'formnovalidate',
+      'formtarget',
+      'height',
+      'incremental',
+      'list',
+      'max',
+      'maxlength',
+      'min',
+      'minlength',
+      'multiple',
+      'name',
+      'pattern',
+      'placeholder',
+      'readonly',
+      'results',
+      'required',
+      'size',
+      'src',
+      'step',
+      'type',
+      'value',
+      'width',
+      'crossOrigin',
+      'formAction',
+      'formEnctype',
+      'formMethod',
+      'formNoValidate',
+      'formTarget',
+      'maxLength',
+      'minLength',
+      'readOnly',
+    ]
+  );
 
-export var DatePicker: DatePickerComponent = (attrsAndProps) => {
-  var textInputRef: HTMLInputElement = null as any;
-  var dateInputRef: HTMLInputElement = null as any;
+  // var [focused, setFocused] = createSignal(false);
 
-  var [customAttrs, rest] = splitProps(attrsAndProps, [
-    'classList',
-    'ref',
-    '$ServerOnly',
-    'children',
-  ]);
+  var dayInputRef: HTMLDivElement = null as any;
+  var monthInputRef: HTMLDivElement = null as any;
+  var yearInputRef: HTMLDivElement = null as any;
+  var buttonRef: HTMLDivElement = null as any;
 
-  // const [value, setValue] = createSignal(rest?.value || '');
-  const [stringDateValue, setStringDateValue] = createSignal('');
+  var placeholder = () => {
+    return inputAttr?.placeholder || '';
+  };
 
-  var inputMode: JSX.HTMLAttributes<HTMLInputElement>['inputMode'] = 'numeric';
+  var DayInput = (attrsAndProps) => {
+    return (
+      <div
+        class="solid-js-date-picker__input"
+        ref={(element) => {
+          dayInputRef = element;
 
-  // createEffect(
-  //   on(
-  //     () => textInputRef.value,
-  //     (v) => {
-  //       console.log(11111, v);
-  //     }
-  //   )
-  // );
+          if (attrsAndProps?.ref != null) {
+            Reflect.apply(attrsAndProps.ref as Function, undefined, [element]);
+          }
+        }}
+        contenteditable
+      >
+        dd
+      </div>
+    );
+  };
 
-  <Portal
-    ref={(el) => {
-      el.classList.add('solid-js-date-picker-children-container');
-    }}
-  >
-    <input
-      type="date"
-      // value={value()}
-      list={rest?.list}
-      ref={(el) => {
-        dateInputRef = el;
-      }}
-      onChange={(event) => {
-        console.log('input date "change" event:', {
-          event,
-          target: event.target,
-        });
-      }}
-    />
+  var MonthInput = (attrsAndProps) => {
+    return (
+      <div
+        class="solid-js-date-picker__input"
+        ref={(element) => {
+          monthInputRef = element;
 
-    <div>test date -&gt; 2024-07-18</div>
+          if (attrsAndProps?.ref != null) {
+            Reflect.apply(attrsAndProps.ref as Function, undefined, [element]);
+          }
+        }}
+        contenteditable
+      >
+        mm
+      </div>
+    );
+  };
 
-    {typeof customAttrs?.children === 'function'
-      ? customAttrs.children()
-      : customAttrs?.children}
-  </Portal>;
+  var YearInput = (attrsAndProps) => {
+    return (
+      <div
+        class="solid-js-date-picker__input"
+        ref={(element) => {
+          yearInputRef = element;
+
+          if (attrsAndProps?.ref != null) {
+            Reflect.apply(attrsAndProps.ref as Function, undefined, [element]);
+          }
+        }}
+        contenteditable
+      >
+        yyyy
+      </div>
+    );
+  };
+
+  var Button = (attrsAndProps) => {
+    return (
+      <button
+        class="solid-js-date-picker__button"
+        type="button"
+        ref={(element) => {
+          buttonRef = element;
+
+          if (attrsAndProps?.ref != null) {
+            Reflect.apply(attrsAndProps.ref as Function, undefined, [element]);
+          }
+        }}
+      >
+        🗓️
+      </button>
+    );
+  };
+
+  var DefaultChildren = (
+    <>
+      {DayInput}
+      <div style={{ 'margin-inline': '0.2em' }}>/</div>
+      {MonthInput}
+      <div style={{ 'margin-inline': '0.2em' }}>/</div>
+      {YearInput}
+      {Button}
+    </>
+  );
 
   return (
-    <input
-      type="text"
-      {...rest}
-      $ServerOnly={customAttrs?.$ServerOnly}
-      classList={customAttrs?.classList}
-      inputmode={rest?.inputmode || inputMode}
-      inputMode={rest?.inputmode || inputMode}
-      class={`${rest?.class || ''} solid-js-date-picker`}
-      ref={(el) => {
-        var proxyfiedEl = new Proxy(el, {
-          get(target, property, receiver) {
-            console.log('get:', target, property, receiver, arguments);
+    <div
+      {...(rest as any)}
+      classList={{
+        ...customAttr?.classList,
+        get ['solid-js-date-picker_placeholder']() {
+          var value = placeholder();
 
-            if (
-              property === 'showPicker' ||
-              property === 'stepDown' ||
-              property === 'stepUp'
-            ) {
-              return dateInputRef[property].bind(dateInputRef);
-            }
-
-            if (
-              property === 'value' ||
-              property === 'valueAsDate' ||
-              property === 'valueAsNumber'
-            ) {
-              return Reflect.get(dateInputRef, property, dateInputRef);
-            }
-
-            var value = Reflect.get(target, property, target);
-
-            if (typeof value === 'function') {
-              return target[property].bind(target);
-            }
-
-            return value;
-          },
-          set(target, property, value, receiver) {
-            console.log('set:', { target, property, value, receiver });
-
-            if (property === 'value') {
-              return Reflect.set(dateInputRef, property, value, dateInputRef);
-            }
-
-            if (property === 'valueAsDate') {
-              return Reflect.set(dateInputRef, property, value, dateInputRef);
-            }
-
-            if (property === 'valueAsNumber') {
-              return Reflect.set(dateInputRef, property, value, dateInputRef);
-            }
-
-            return Reflect.set(target, property, value, receiver);
-          },
-        });
-
-        textInputRef = proxyfiedEl;
-        window.datePickerRef1 = proxyfiedEl;
-
-        if (customAttrs?.ref != null) {
-          Reflect.apply(customAttrs?.ref as Function, undefined, [proxyfiedEl]);
-        }
-      }}
-      // value={value()}
-      onChange={(event) => {
-        dateInputRef.value = event.target.value;
-
-        // setValue(event.target.value);
-
-        // console.log({
-        //   'dateInputRef.value': dateInputRef.value,
-        //   'dateInputRef.valueAsNumber': dateInputRef.valueAsNumber,
-        //   'dateInputRef.valueAsDate': dateInputRef.valueAsDate,
-        // });
-      }}
-      onPaste={(event) => {
-        event.preventDefault();
-
-        if (rest?.onPaste != null) {
-          if (typeof rest.onPaste === 'function') {
-            rest.onPaste(event);
+          if (value == null) {
+            return false;
           }
 
-          if (isArray(rest.onPaste)) {
-            //
+          if (value === '') {
+            return false;
+          }
+
+          return true;
+        },
+      }}
+      $ServerOnly={customAttr?.$ServerOnly}
+      class={`${rest?.class || ''} solid-js-date-picker1`}
+      data-placeholder={placeholder()}
+      onFocusIn={(event) => {
+        console.group('"onFocusIn"');
+        console.log('event:', event);
+        console.log('event.target:', event.target, { e: event.target });
+        console.groupEnd();
+
+        // ============================ SELECTION ============================
+        const range = document.createRange();
+        range.selectNodeContents(event.target);
+        const selection = window.getSelection();
+
+        if (selection != null) {
+          selection.removeAllRanges();
+          selection.addRange(range);
+        }
+        // ============================ SELECTION ============================
+
+        // event.target.classList.remove('solid-js-date-picker_placeholder');
+      }}
+      // onFocusOut={(event) => {
+      //   console.log('"onFocusOut" event:', event);
+      // }}
+      onInput={(event) => {
+        console.group('"onInput"');
+        console.log('event:', event);
+        console.log('event.target:', event.target, { e: event.target });
+        console.groupEnd();
+
+        var target = event.target;
+        var textContent = target.textContent!;
+
+        if (target === dayInputRef) {
+          // ============================ SELECTION ============================
+          var previousTextContent = textContent;
+
+          const range = document.createRange();
+          range.selectNodeContents(event.target);
+          const selection = window.getSelection();
+
+          if (selection != null) {
+            selection.removeAllRanges();
+            selection.addRange(range);
+          }
+
+          event.target.textContent! =
+            event.target.textContent! + previousTextContent;
+          // ============================ SELECTION ============================
+
+          if (textContent.length > 2) {
+            event.preventDefault();
+
+            return false;
+          }
+        }
+
+        if (target === monthInputRef) {
+          if (textContent.length > 2) {
+            event.preventDefault();
+
+            return false;
+          }
+        }
+
+        if (target === yearInputRef) {
+          if (textContent.length > 4) {
+            event.preventDefault();
+
+            return false;
           }
         }
       }}
-      list={null as any}
-    />
+    >
+      <input
+        class="solid-js-date-picker__input-date"
+        type="date"
+        tabindex={9999}
+      />
+
+      <Show
+        when={customAttr?.children == null}
+        fallback={
+          <Show
+            when={typeof customAttr.children! === 'function'}
+            fallback={DefaultChildren}
+          >
+            {(customAttr as any).children({
+              DayInput,
+              MonthInput,
+              YearInput,
+              Button,
+            })}
+          </Show>
+        }
+      >
+        {DefaultChildren}
+      </Show>
+    </div>
   );
-};
+}) as Component<
+  Omit<
+    JSX.IntrinsicElements['input'],
+    /* ------------------------- overwritten attrs ------------------------- */
+    'children'
+    /* ------------------------- overwritten attrs ------------------------- */
+  > & {
+    children?:
+      | JSX.Element
+      | ((args: {
+          DayInput: Component<any>;
+          MonthInput: Component<any>;
+          YearInput: Component<any>;
+          Button: Component<any>;
+        }) => JSX.Element);
+  }
+>;
