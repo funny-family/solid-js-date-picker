9329bd8aeb0b792914bb9f94a30c71b139df6fff
funny-family <maksvadik228@gmail.com>
2024-07-26 10:36:32 +0700

    update

--- playground/src/app.component.tsx
+++ playground/src/app.component.tsx
@@ -4,174 +4,76 @@ import {
   type JSX,
   Show,
   createSignal,
+  createEffect,
 } from 'solid-js';
-import { DatePicker, DatePickerForwardElement } from './solid-js-date-picker';
-import './app.styles.css';
-import {
-  Picker,
-  pickerExposeSymbol,
-} from '../../lib/component/components/picker/picker.component';
-import { PickerRef } from '../../lib/component/components/picker/picker.component.types';
-import { TestDatePicker } from './test-date-picker';
+import { DatePicker } from './date-picker';
+import { DatePicker as DatePicker1 } from './date-picker1/date-picker.component';
 
-window.pickerExposeSymbol = pickerExposeSymbol;
+var DP = (attrsAndProps) => {
+  var dateInputRef = attrsAndProps?.ref as any;
+  // const [dateInputRef, setDateInputRef] = createSignal(second)
+
+  return (
+    <input
+      type="date"
+      ref={(el) => {
+        dateInputRef = el;
+      }}
+    />
+  );
+};
 
 export const App: Component = () => {
-  var datePickerRef: DatePickerForwardElement = null as any;
+  var datePickerRef = null as any;
 
-  let pickerRef1: PickerRef = null as any;
-  let pickerRef2: PickerRef = null as any;
-
-  var [isPickerRef2Visible, setPickerRef2Visibility] = createSignal(false);
-
-  var formatString = 'MM.DD.YYYY' as const;
-
-  onMount(() => {
-    // console.log(234234, { ref });
+  createEffect(() => {
+    console.log(12313131, { datePickerRef });
+    window.datePickerRef = datePickerRef;
   });
 
   return (
     <main>
-      <section>
-        <h1>DatePicker</h1>
-        <div>
-          <br />
+      <DatePicker
+        min="2018-01-01"
+        max="2024-12-31"
+        name="123131"
+        value="2018-12-12"
+        autocomplete="YEAR"
+        ref={(el) => {
+          console.log(12313131, el);
 
-          <DatePicker
-            // children={null}
-            class="a6dadgauydag"
-            format={formatString}
-            placeholder={formatString}
-            ref={(el) => {
-              datePickerRef = el;
-            }}
-            // disabled
-            // keepNativePicker
-            onOpen={(event) => {
-              console.log('open event:', event);
-            }}
-            onClose={(event) => {
-              console.log('close event:', event);
-            }}
-          >
-            {() => {
-              return (
-                <button
-                  type="button"
-                  onClick={() => {
-                    console.log({ datePickerRef });
+          datePickerRef = el;
+        }}
+        // ref={datePickerRef}
+        // children={({ TextInput, DateInput }) => {
+        //   return (
+        //     <>
+        //       <div>1</div>
+        //       <TextInput />
+        //       <DateInput />
+        //       <div>2</div>
+        //     </>
+        //   );
+        // }}
+      />
 
-                    datePickerRef.showPicker();
-                  }}
-                >
-                  🗓️
-                </button>
-              );
-            }}
-          </DatePicker>
+      <hr />
 
-          <hr />
+      <div style={{ height: '50px' }} />
 
-          <section>
-            <h1>Visible Picker</h1>
+      <DatePicker1 placeholder="dd-mm-yyyy" />
 
-            <Picker
-              // ref={pickerRef}
-              ref={(el) => {
-                pickerRef1 = el;
-              }}
-              style={{ border: '1px solid black' }}
-              onOpen={() => {
-                console.log('Picker is open!');
-              }}
-              onClose={() => {
-                console.log('Picker is close!');
-              }}
-            >
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Architecto quam quo iste nesciunt nemo molestias, labore
-                dolores, error incidunt, obcaecati beatae. Enim odio nostrum
-                sequi! Placeat id magnam sit esse.
-              </p>
-              <button
-                type="button"
-                onClick={() => {
-                  pickerRef1[pickerExposeSymbol].close();
-                }}
-              >
-                close
-              </button>
-            </Picker>
-            <button
-              type="button"
-              onClick={() => {
-                pickerRef1[pickerExposeSymbol].open();
-              }}
-            >
-              open
-            </button>
-          </section>
+      {/* <DatePicker1 style={{ margin: '4em' }}>
+        {() => {
+          return 1111;
+        }}
+      </DatePicker1> */}
 
-          <hr />
-
-          <section>
-            <h1 onClick={() => setPickerRef2Visibility(false)}>
-              Non Visible Picker
-            </h1>
-
-            <Show when={isPickerRef2Visible()} fallback={null}>
-              <Picker
-                ref={pickerRef2}
-                // ref={(el) => {
-                //   pickerRef2 = el;
-                // }}
-                style={{ border: '1px solid pink' }}
-                onOpen={() => {
-                  console.log('Picker is open!');
-                }}
-                onClose={() => {
-                  console.log('Picker is close!');
-                  setPickerRef2Visibility(false);
-                }}
-              >
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Architecto quam quo iste nesciunt nemo molestias, labore
-                  dolores, error incidunt, obcaecati beatae. Enim odio nostrum
-                  sequi! Placeat id magnam sit esse.
-                </p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    pickerRef2[pickerExposeSymbol].close();
-                  }}
-                >
-                  close
-                </button>
-              </Picker>
-            </Show>
-
-            <button
-              type="button"
-              onClick={() => {
-                setPickerRef2Visibility(true);
-                pickerRef2[pickerExposeSymbol].open();
-              }}
-            >
-              open
-            </button>
-          </section>
-        </div>
-      </section>
-
-      <section>
-        <h1>Wrapped style DatePicker</h1>
-
-        <div>
-          <TestDatePicker />
-        </div>
-      </section>
+      {/* <DP
+        ref={(el) => {
+          console.log(989898, el);
+        }}
+      /> */}
     </main>
   );
 };
